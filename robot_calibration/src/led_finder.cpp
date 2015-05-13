a21e8e31c429724da787796c4523e2d5eb29f98a
varunns <varunns@vt.edu>
2015-05-12 19:32:41 -0700

    equal weighted pixels, blob is visible, testing to see if max pixel can be identified using cv::minMaxLoc

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -487,20 +487,19 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   double *maxVal = new double();
   cv::Point *minLoc = new cv::Point(); 
   cv::Point *maxLoc = new cv::Point();
-  cv::minMaxLoc(diff_sum_image, minVal, maxVal, minLoc, maxLoc);
   debug_img(diff_sum_image,"/tmp/mean/diff_", 0, 0, 0);
 
   //split channels
-  /*std::vector<cv::Mat> channels(3);
+  std::vector<cv::Mat> channels(3);
   cv::split(diff_sum_image, channels);
   cv::minMaxLoc(channels[0], minVal, maxVal, minLoc, maxLoc);
   cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(0,0,255), 1, 8);
-  *//*cv::minMaxLoc(channels[1], minVal, maxVal, minLoc, maxLoc);
+  cv::minMaxLoc(channels[1], minVal, maxVal, minLoc, maxLoc);
   cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(0,255,0), 1, 8);
   cv::minMaxLoc(channels[2], minVal, maxVal, minLoc, maxLoc);
-  cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(255,0,0), 1, 8);*/
+  cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(255,0,0), 1, 8);
 
-//  debug_img(cloud_image_ptr[0]->image, "/tmp/mean/image_", 0, 0, 0);
+  debug_img(cloud_image_ptr[0]->image, "/tmp/mean/image_", 0, 0, 0);
 
  /* calculating the weighted sum*/
 

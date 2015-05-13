f74bf1cb1db507ec58cdfcccd4e6b9a92c29f678
varunns <varunns@vt.edu>
2015-05-12 17:09:44 -0700

    debugging

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -492,10 +492,10 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   cv::split(diff_sum_image, channels);
   cv::minMaxLoc(channels[0], minVal, maxVal, minLoc, maxLoc);
   cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(0,0,255), 1, 8);
-  cv::minMaxLoc(channels[1], minVal, maxVal, minLoc, maxLoc);
+  /*cv::minMaxLoc(channels[1], minVal, maxVal, minLoc, maxLoc);
   cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(0,255,0), 1, 8);
   cv::minMaxLoc(channels[2], minVal, maxVal, minLoc, maxLoc);
-  cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(255,0,0), 1, 8);
+  cv::circle(cloud_image_ptr[0]->image, *maxLoc, 10, cv::Scalar(255,0,0), 1, 8);*/
 
   debug_img(cloud_image_ptr[0]->image, "/tmp/mean/image_", 0, 0, 0);
 

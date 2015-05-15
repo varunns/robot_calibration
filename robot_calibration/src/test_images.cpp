2e94293ff9c56bd9a4eb861c42f21295154698c8
varunns <varunns@vt.edu>
2015-05-15 15:13:52 -0700

    testing

--- robot_calibration/src/test_images.cpp
+++ robot_calibration/src/test_images.cpp
@@ -80,7 +80,7 @@ public:
     seg.setOptimizeCoefficients (false);
     seg.setModelType (pcl::SACMODEL_PLANE);
     seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.1);
+    seg.setDistanceThreshold (0.07);
     seg.setInputCloud (pcl_cloud);
     seg.segment (*inliers, *coefficients);
      //extract indices\

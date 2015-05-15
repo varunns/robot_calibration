15e355e20c5558930063936a7973611436d3375a
varunns <varunns@vt.edu>
2015-05-14 22:52:56 -0700

    testing

--- robot_calibration/src/test_images.cpp
+++ robot_calibration/src/test_images.cpp
@@ -24,12 +24,14 @@ private:
   bool flag_;
   int i;
   std::vector<cv::Mat> images;
+  cv::Scalar diff;
 public:
   TestImages()
   {
     sub_ = nh_.subscribe("/head_camera/rgb/image_rect_color", 1, &TestImages::imageCB, this);
     flag_ = true;
     i = 0;
+    diff = cv::Scalar(0,0,0,0);
 
   }
 
@@ -62,16 +64,14 @@ public:
   {
    
     cv::Mat diff_image;
-    cv::Scalar diff = cv::Scalar(0,0,0,0);
     for(int i = 1; i < images.size(); i++)
     {
       cv::absdiff(images[i], images[i-1],diff_image);
       cv::Scalar mean_diff = cv::mean(diff_image);
-      std::cout<<mean_diff<<" ::::: "<< (mean_diff - diff)<<std::endl;
+      std::cout<<mean_diff - diff<<std::endl;
       diff = mean_diff;
       debug_img(diff_image, "/tmp/mean/image_", 0, 0, 0);
     }
-    std::cout<<"**************************************************************"<<std::endl;
   }
 
 void debug_img(cv::Mat image, std::string string_in, int k, int l, float diff)

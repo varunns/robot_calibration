f53e7532cb6023b6002e0ee0d19aecf82eb046b1
varunns <varunns@vt.edu>
2015-05-20 11:48:11 -0700

    testing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -486,9 +486,9 @@ void LedFinder::CloudDifferenceTracker::differenceImage(cv::Mat image1, cv::Mat 
   cv::Mat imcovar;
   
   double min_mahala_dist = 1000;
-  cv::Mat tmp;
-  cv::Mat tmp_thresh;
-  cv::cvtColor(image1, tmp_thresh, CV_BGR2GRAY);
+  cv::Mat tmp_image;
+  cv::Mat gray_image;
+  cv::cvtColor(image1, gray_image, CV_BGR2GRAY);
   /*cv::threshold(tmp_thresh, tmp, 175, 255, CV_THRESH_BINARY);
   cv::Canny( tmp, canny, 20, 20*3, 3 );*/
   //cv::cvtColor(tmp, img, CV_GRAY2BGR);

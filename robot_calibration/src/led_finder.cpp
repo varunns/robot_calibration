a401be39b87c0dfd5920035e2cf8c94c16d17a54
varunns <varunns@vt.edu>
2015-05-20 02:30:35 -0700

    testing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -445,7 +445,8 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   cv::Mat diff1_pix = cv::Mat(cloud_image_ptr[0]->image.rows, cloud_image_ptr[0]->image.cols, CV_8UC3, cv::Scalar(255,255,255));
   cv::absdiff(prev_pix_weighed, cloud_pix_weighed, diff_pix);
   //calculate the difference Image
- // differenceImage(cloud_pix_weighed, prev_pix_weighed, diff_pix, cloud_pix_weighed);
+  cv::Mat img;
+ differenceImage(cloud_pix_weighed, prev_pix_weighed, diff_pix, img);
 
 /*  double *minVal = new double();
   double *maxVal = new double();
@@ -481,7 +482,7 @@ void LedFinder::CloudDifferenceTracker::differenceImage(cv::Mat image1, cv::Mat 
   int count = 0;
   cv::Mat diff1_image, tmp;
    cv::absdiff(image1, image2, diff1_image);
-   cv::cvtColor(image2, tmp, CV_BGR2GRAY);
+   cv::cvtColor(image1, tmp, CV_BGR2GRAY);
    cv::threshold(tmp, tmp, 175, 255, CV_THRESH_BINARY);
    cv::cvtColor(tmp, img, CV_GRAY2BGR);
   /*for(uint i = 20; i < image1.rows - 20; i++)

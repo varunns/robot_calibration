835e22982280df18a754ca495bfa47761715402f
varunns <varunns@vt.edu>
2015-05-21 08:57:43 -0700

    testing distribution

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -123,7 +123,7 @@ void LedFinder::cameraCallback(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr clou
   { 
     cloud_ptr_ = cloud;
     clouds_ptr_.push_back(cloud);
-    if(clouds_ptr_.size() > 19)
+    if(clouds_ptr_.size() > 34)
     {
       waiting_ = false;
     }
@@ -432,9 +432,9 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
 
   //taking images indexed 6 to 6+8 to avoid starting images
   std::vector<cv::Mat> past_images;
-  for(int i = 0; i < 8; i++)
+  for(int i = 0; i < 15; i++)
   {
-    past_images.push_back((prev_images[i+6])->image);
+    past_images.push_back((prev_images[i+10])->image);
   }
 
   cv::Mat led_image;

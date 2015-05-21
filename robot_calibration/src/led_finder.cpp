4f8fce8563bbdd5c73deac196bd9942a09d4f5c1
varunns <varunns@vt.edu>
2015-05-21 13:04:16 -0700

    testing distribution

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -563,8 +563,8 @@ void LedFinder::CloudDifferenceTracker::differenceImage(std::vector<cv::Mat>& cu
       for(int i = 0; i < past_images.size(); i++)
       {
         int a = (lab[i].at<cv::Vec3b>(k,j))[0];
-        int b = lab[i].at<cv::Vec3b>(k,j)[1];
-        int c = lab[i].at<cv::Vec3b>(k,j)[2];
+        int b = (lab[i].at<cv::Vec3b>(k,j))[1];
+        int c = (lab[i].at<cv::Vec3b>(k,j))[2];
         cv::Scalar val = cv::Scalar(a,b,c,0);
         sum = sum + val;
       }

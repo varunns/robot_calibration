2ef45f09a7b000725903a7d7a2d954c80e855072
varunns <varunns@vt.edu>
2015-05-21 16:57:16 -0700

    testing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -580,11 +580,11 @@ void LedFinder::CloudDifferenceTracker::differenceImage(std::vector<cv::Mat>& cu
       mean = cv::Scalar(sum[0]/kjScalars.size(), sum[1]/kjScalars.size(), sum[2]/kjScalars.size(), 0);
       sum = cv::Scalar(0,0,0,0);
 
-      /*for(int i = 0; i < kjScalars.size(); i++)
+      for(int i = 0; i < kjScalars.size(); i++)
       {
         sum += cv::Scalar( pow( ( (kjScalars[i])[0] - mean[0] ), 2), pow( ( (kjScalars[i])[1] - mean[1] ), 2), pow( ( (kjScalars[i])[2] - mean[2] ), 2), 0 ); 
       }
-      */
+      
       kjScalars.clear();
      // sum = cv::Scalar(sqrt(sum[0]/(past_images.size())), sqrt(sum[1]/(past_images.size())), sqrt(sum[2]/(past_images.size())), 0);
 /*    

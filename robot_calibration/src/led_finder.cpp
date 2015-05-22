fe45ff4c0ab61a4bee40d8e3464607035c7a6ebd
varunns <varunns@vt.edu>
2015-05-22 07:36:06 -0700

    testing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -592,7 +592,7 @@ void LedFinder::CloudDifferenceTracker::differenceImage(std::vector<cv::Mat>& cu
     }
   }
   std::vector<cv::Mat> channels(3);
-  cv::split(past_images, channels);
+  cv::split(past_images[5], channels);
   cv::Mat one = channels[0];
 
   channels.resize(3);

3018e8781f7fc6095df9c1b2394006ddb859507b
varunns <varunns@vt.edu>
2015-05-22 08:49:50 -0700

    testing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -506,7 +506,7 @@ void LedFinder::CloudDifferenceTracker::differenceImage(cv::Mat image1, cv::Mat 
       {
         continue;
       }
-      cv::Rect rect1(j-6, i-6, 15, 15);
+      cv::Rect rect1(j-5, i-5, 10, 10);
       cv::meanStdDev(diff1_image(rect1), mean, std_dev);
       mean_val = pow(mean[0], 2) + pow(mean[1], 2) +pow(mean[2], 2);  
       dev_val = pow(std_dev[0], 2) + pow(std_dev[1], 2) +pow(std_dev[2], 2);  

2a5905815ddea62db9f7d822cdd9031e95ef1754
varunns <varunns@vt.edu>
2015-06-03 10:56:17 -0700

    considering all the 3d points and takking the nearest of all the cloud...continuing....considering the case wer no candidates are available..testing for max candidate....tuning...corrected the scalar inputs for sum

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -681,7 +681,7 @@ void LedFinder::CloudDifferenceTracker::possibleContours(cv::Mat& diff_image, st
   std::vector<cv::Vec4i> hierarchy;
   cv::Mat gray;
   cv::cvtColor(diff_image, gray, CV_BGR2GRAY);
-  cv::Canny(diff_image, canny_image, canny_thresh, canny_thresh*2, 1);
+  cv::Canny(diff_image, canny_image, canny_thresh, canny_thresh*2, 3);
   cv::findContours(canny_image, contours, hierarchy,CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 }
 

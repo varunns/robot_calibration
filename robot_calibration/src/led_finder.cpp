066f8cbd0871d3c6a4acf261943f5ca747692762
varunns <varunns@vt.edu>
2015-06-03 09:32:15 -0700

    considering all the 3d points and takking the nearest of all the cloud...continuing....considering the case wer no candidates are available..testing for max candidate....debugging

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -508,11 +508,12 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   {
     for( int j = 0 ; j < final_contours.size(); j++)
     {
-    //  cv::Point pt = (final_contours[j])[0];
+      cv::Point pt = (final_contours[j])[0];
 
       cv::Mat gray_cloud;
       cv::Mat gray_prev;
       cv::Mat gray_diff;
+
       cv::Rect roi = cv::Rect(pt.x-5,pt.y-5, 10, 10);
       cv::cvtColor(cloud_pix_weighed(roi), gray_cloud, CV_BGR2GRAY);
       cv::cvtColor(prev_pix_weighed(roi), gray_prev, CV_BGR2GRAY);
@@ -527,7 +528,7 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
         max = sums;
         max_pt = cv::Point(pt.x-5,pt.y-5);
       }
-      cv::Point pt = (final_contours[j])[0];
+     // cv::Point pt = (final_contours[j])[0];
       cv::rectangle(cloud_pix_weighed, cv::Rect(pt.x-5,pt.y-5, 10, 10), cv::Scalar(0,0,255), 1, 8);
       /*cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
       cv::drawContours(diff_image, final_contours, j, color, 1, 8, cv::noArray(), 0, cv::Point());*/

638baca782a2376b72ce06a96999f708f574b313
varunns <varunns@vt.edu>
2015-06-03 03:47:14 -0700

    final elimmination of candidate regions

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -509,7 +509,7 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   {
     for( int j = 0 ; j < final_contours.size(); j++)
     {
-      cv::Point pt = (final_contours[j])[0];
+   /*   cv::Point pt = (final_contours[j])[0];
 
       cv::Mat gray;
       cv::Rect roi =  cv::Rect(pt.x-5,pt.y-5, 10, 10);
@@ -530,11 +530,11 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
       {
         max2 = sums;
         max2_pt = cv::Point(pt.x-5,pt.y-5);
-      }
-     // cv::Point pt = (final_contours[j])[0];
-//      cv::rectangle(cloud_pix_weighed, cv::Rect(max1_pt.x-5,max1_pt.y-5, 20, 20), cv::Scalar(0,0,255), 1, 8);
-/*    cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-      cv::drawContours(diff_image, final_contours, j, color, 1, 8, cv::noArray(), 0, cv::Point());*/
+      }*/
+     cv::Point pt = (final_contours[j])[0];
+     cv::rectangle(cloud_pix_weighed, cv::Rect(max1_pt.x-5,max1_pt.y-5, 20, 20), cv::Scalar(0,0,255), 1, 8);
+    cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      cv::drawContours(diff_image, final_contours, j, color, 1, 8, cv::noArray(), 0, cv::Point());
     }
   }
   cv::rectangle(cloud_pix_weighed, cv::Rect(max1_pt.x-2,max1_pt.y-2, 10, 10), cv::Scalar(0,0,255), 1, 8);

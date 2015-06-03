73f60cf96313eb543d35478aff8810fba6adc622
varunns <varunns@vt.edu>
2015-06-03 05:02:51 -0700

    final elimmination of candidate regions

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -548,7 +548,7 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
   }
   std::cout<<x/total<<" "<<y/total<<" "<<std::endl;\
   if(total >0)
-    cv::circle(diff_image, cv::Point(std::floor(x/total),std::floor(y/total)),3,cv::Scalar(0,0,255), 2,8,0);
+    cv::circle(diff_image, cv::Point(std::floor(x/total),std::floor(y/total)),5,cv::Scalar(0,0,255), 2,8,0);
   //getting the center of LED and searching for the location method 2, using the difference image
   debug_img(diff_image, "/tmp/mean/contourimage_", 0,0,0);
   debug_img(cloud_pix_weighed, "/tmp/mean/colorimage_",0,0,0);

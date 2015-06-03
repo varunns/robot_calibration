b122a6cd14afadc39a65a1c4a1d19aa97b6ff71e
varunns <varunns@vt.edu>
2015-06-03 08:26:07 -0700

    considering all the 3d points and takking the nearest of all the cloud...continuing....changed the top priority..avoiding seg faults from lack of contours_removed gaussian blur

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -660,12 +660,7 @@ void LedFinder::CloudDifferenceTracker::possibleContours(cv::Mat& diff_image, st
   std::vector<cv::Vec4i> hierarchy;
   cv::Mat gray;
   cv::cvtColor(diff_image, gray, CV_BGR2GRAY);
-  /*cv::threshold(gray, gray, 10, 255, CV_THRESH_BINARY);
-  cv::Canny(gray, canny_image, canny_thresh, canny_thresh*2, 3);
-  */
-  cv::blur( gray, gray, cv::Size(3,3) );
-  cv::Canny(gray, canny_image, canny_thresh, canny_thresh*2, 3);
-  //cv::Canny(diff_image, canny_image, canny_thresh, canny_thresh*2, 3);
+  cv::Canny(diff_image, canny_image, canny_thresh, canny_thresh*2, 3);
   cv::findContours(canny_image, contours, hierarchy,CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 }
 

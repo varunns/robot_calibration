abacdeed04b3564c31f8bfaebb10fd95df2c148d
varunns <varunns@vt.edu>
2015-06-02 23:18:55 -0700

    testing q

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -504,7 +504,7 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
 
   else
   {
-    for( int j = 0 ; j < final_contours.size(); j++)
+    for( int j = 0 ; j < 5; j++)
     {
       cv::Point pt = (final_contours[j])[0];
       cv::rectangle(diff_image, cv::Rect(pt.x-5,pt.y-5, 10, 10), cv::Scalar(0,0,255), 1, 8);

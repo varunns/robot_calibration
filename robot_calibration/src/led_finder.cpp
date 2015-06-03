fe4bbedf46aba10637af0cfadc265eb00fb0c6ec
varunns <varunns@vt.edu>
2015-06-02 19:57:18 -0700

    checking draw contours with single contour

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -498,11 +498,13 @@ bool LedFinder::CloudDifferenceTracker::oprocess(
 
   else
   {
-    for( int j = 0 ; j < final_contours.size(); j++)
+      cv::Scalar color = cv::Scalar( 0,0,255 );
+      cv::drawContours(diff_image, final_contours[0], 0, color, 1, 8, cv::noArray(), 0, cv::Point());
+ /*   for( int j = 0 ; j < final_contours.size(); j++)
     {
       cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
       cv::drawContours(diff_image, final_contours[j], j, color, 1, 8, cv::noArray(), 0, cv::Point());
-    }
+    }*/
   }
 
   debug_img(diff_image, "/tmp/mean/contourimage_", 0,0,0);

46541da900fe7d3094c9a2dd810f4fa67c893aab
varunns <varunns@vt.edu>
2015-06-03 19:44:58 -0700

    changed asyncspinner back to 1

--- robot_calibration/src/calibrate.cpp
+++ robot_calibration/src/calibrate.cpp
@@ -88,7 +88,7 @@ int main(int argc, char** argv)
     pose_bag_name = argv[1];
 
   // All ROS callbacks are processed in a separate thread
-  ros::AsyncSpinner spinner(1);
+  ros::AsyncSpinner spinner(2);
   spinner.start();
 
   if (pose_bag_name.compare("--from-bag") != 0)

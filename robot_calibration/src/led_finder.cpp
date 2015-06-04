c5b937bf2e94017811bbcdfacd799e33ffbcb313
varunns <varunns@vt.edu>
2015-06-04 08:07:55 -0700

    testing for all leds

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -290,12 +290,10 @@ bool LedFinder::find(robot_calibration_msgs::CalibrationData * msg)
 
   //-------------------------------------------------------------------------------------------Strart Using the populated vector of sharedPtr : led_respective_contours
   std::vector<pcl::PointXYZRGB> led_pts;
-  //for( int i = 0 ; i < led_respective_contours.size(); i++)
-  //for( int i = 2 ; i < 3; i++)
+  for( int i = 0 ; i < led_respective_contours.size(); i++)
   {
-    std::cout<<"in the loop"<<std::endl;
     pcl::PointXYZRGB temp_led;
-    getCandidateRoi(led_respective_contours[3], temp_led);
+    getCandidateRoi(led_respective_contours[i], temp_led);
     led_pts.push_back(temp_led);
   }
 
@@ -442,6 +440,7 @@ void LedFinder::getCandidateRoi(CloudDifferenceTracker::TrackContoursPtr tracker
 
     }
   }
+
 
   //debug
   for( int i = 0; i < matched_contours.size(); i++)

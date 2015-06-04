029e9da62ee2d5b64c0af031035d55bab59da64e
varunns <varunns@vt.edu>
2015-06-04 09:40:03 -0700

    testing and

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -412,12 +412,13 @@ void LedFinder::getCandidateRoi(CloudDifferenceTracker::TrackContoursPtr tracker
   cv::Mat dst;
   for(int i = 1; i < (tracker_in->diff_images).size(); i++)
   {
-    bitwise_and((tracker_in->diff_images)[i], tmp, dst);
+    cv::bitwise_and((tracker_in->diff_images)[i], tmp, dst);
     tmp = dst;
 //    localDebugImage((tracker_in->rgb_image)[i], "/tmp/mean/image_");
     localDebugImage((tracker_in->diff_images)[i], "/tmp/mean/diff_");
   }
   localDebugImage(dst,"tmp/mean/bitwise_");
+
 
   //Vector of contours that have matches TODO should be made a boost::share_ptr
 

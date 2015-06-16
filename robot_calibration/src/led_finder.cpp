4e464165fd16ede89af6dc0d4fa58fb4b7b4865e
varunns <varunns@vt.edu>
2015-06-16 03:28:18 -0700

    checking 3d points with threshold as 100

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -539,10 +539,10 @@ void LedFinder::getCandidateRoi(CloudDifferenceTracker::TrackContoursPtr& tracke
     std::cout<<"bounds: "<<bounds.tl().x<<" "<<bounds.tl().y<<std::endl;
     std::cout<<"centers: "<<center_x<<" "<<center_y<<std::endl;
     pcl::PointXYZRGB pt3;
-    cv::rectangle(tracker_in->diff_images[1], cv::Rect(round(center_x) -4, round(center_y) -4, 8, 8), cv::Scalar(0,0,255), 1, 8);
-    for( size_t i = round(center_x) - 4; i < round(center_x) + 4; i++)
+    cv::rectangle(tracker_in->diff_images[1], cv::Rect(round(center_x) -6, round(center_y) -6, 12, 12), cv::Scalar(0,0,255), 1, 8);
+    for( size_t i = round(center_x) - 6; i < round(center_x) + 6; i++)
     {
-      for( size_t j = round(center_y) - 4; j < round(center_y) + 4; j++)
+      for( size_t j = round(center_y) - 6; j < round(center_y) +6; j++)
       {
         int val = (int)diff_gray.at<uchar>(j,i);
         if( val > 100)

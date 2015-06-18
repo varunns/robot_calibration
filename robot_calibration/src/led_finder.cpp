a179057d2e0d12c76e7b65761c03bfbbde512fa1
varunns <varunns@vt.edu>
2015-06-17 18:25:39 -0700

    remove transform printing

--- robot_calibration/src/led_finder.cpp
+++ robot_calibration/src/led_finder.cpp
@@ -329,7 +329,7 @@ bool LedFinder::find(robot_calibration_msgs::CalibrationData * msg)
     try
     {
       listener_.transformPoint(trackers_[t].frame_, ros::Time(0), rgbd_pt,
-                               "/head_camera_rgb_optical_frame", world_pt);
+                               "/head_camera_depth_optical_frame", world_pt);
     }
     
     catch(const tf::TransformException &ex)
